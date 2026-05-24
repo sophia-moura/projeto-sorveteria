25558330975ca4b2b0906c3507665c8e0a5af848
☆lice <xavieraliceh@gmail.com>
2026-05-24 19:02:32 -0300

    Refactor Milkshake class with additional features

--- Milkshake.java
+++ Milkshake.java
@@ -1,18 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.produtos;
+package model;
 
 public class Milkshake extends Produtos {
+
     private int ml;
 
-    public Milkshake(String nome, double preco, int ml) {
-        super(nome, preco);
+    public Milkshake(int id, String nome, double preco, int estoque, int ml) {
+        super(id, nome, "Milkshake", preco, estoque);
         this.ml = ml;
     }
 
     public int getMl() { return ml; }
     public void setMl(int ml) { this.ml = ml; }
-}
 
+    @Override
+    public double calcularPreco() {
+        // Milkshake tem acréscimo de 10% por ser maior
+        return preco * 1.10;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Volume: " + ml + "ml";
+    }
+}
