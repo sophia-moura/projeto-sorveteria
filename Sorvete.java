647a760ced39b87075ad5514025d82368d04c969
☆lice <xavieraliceh@gmail.com>
2026-05-24 19:06:42 -0300

    Update Sorvete.java

--- Sorvete.java
+++ Sorvete.java
@@ -1,15 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.produtos;
+package model;
 
-public class Sorvete extends Produtos {
+public class Sorvete extends Produto {
+
     private String sabor;
 
-    public Sorvete(String nome, double preco, String sabor){
-        super(nome, preco);
+    public Sorvete(int id, String nome, double preco, int estoque, String sabor) {
+        super(id, nome, "Sorvete", preco, estoque);
         this.sabor = sabor;
     }
-}
 
+    public String getSabor() { return sabor; }
+    public void setSabor(String sabor) { this.sabor = sabor; }
+
+    @Override
+    public double calcularPreco() {
+        // Sorvete não tem acréscimo
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Sabor: " + sabor;
+    }
+}
